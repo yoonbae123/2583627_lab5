70aff009713366064dd6105bc8ed263f0ae365f3
YB <2583627@students.wits.ac.za>
2025-03-17 16:22:25 +0200

    final

--- 2583627_lab5.js
+++ 2583627_lab5.js
@@ -26,10 +26,10 @@ app.get('/books/:id',(req,res)=>{
 
 app.post('/books',(req,res)=>{
   const newBook = req.body;
+  if (!newBook.title || !Array.isArray(newBook.details)) {
+    return res.status(400).json({error:"Missing required book details"});}
   newBook.id = books.length + 1;
   newBook.details = newBook.details || []; 
-  if (!newBook.title) {
-    return res.status(400).json({error:"title is required"});}
   books.push(newBook);
   res.status(201).json(newBook);
 });
@@ -40,9 +40,8 @@ app.put('/books/:id',(req,res)=>{
   if (bookIndex===-1) {
     return res.status(404).json({error:"not found"});
   }else{
-    const book = req.body;
-    books[bookIndex] = book;
-    res.status(200).json(book);
+    books[bookIndex] = { ...books[bookIndex], ...req.body, id: bookID };
+    res.status(200).json(books[bookIndex]);
   }
 });
 
@@ -51,31 +50,32 @@ app.delete('/books/:id',(req, res) => {
   const bookIndex = books.findIndex(b=>b.id===bookID);
   if (bookIndex===-1){
     return res.status(404).json({error:"not found"});}
-  books.splice(bookIndex,1);
-  res.status(204).send(); 
+    books.splice(bookIndex, 1);
+    res.status(200).json({message:"Successfully deleted"});
 });
 
-app.post('/books/:id/details',(req,res)=>{
+app.post('/books/:id/details',(req, res)=>{
   const bookID = parseInt(req.params.id);
-  const book = books.find(b=>b.id===bookID);
+  const book = books.find(b=>b.id === bookID);
   if (!book) {
-    return res.status(404).json({error:"not found"});}
-  const newDetail = req.body;
+      return res.status(404).json({error:"not found"});}
+  const newDetail=req.body;
+  newDetail.id=(book.details.length+1).toString();
   book.details.push(newDetail);
   res.status(201).json(newDetail);
 });
 
-app.delete('/books/:id/details/:detailID',(req,res)=>{
+app.delete('/books/:id/details/:detailID', (req, res) => {
   const bookID = parseInt(req.params.id);
   const detailID = req.params.detailID;
-  const book = books.find(b=>b.id===bookID);
-  if (!book){
+  const book = books.find(b => b.id === bookID);
+  if(!book){
       return res.status(404).json({error:"Book not found"});}
-  const detailIndex =book.details.findIndex(d=>d.id===detailID);
-  if (detailIndex=== -1) {
+  const detailIndex = book.details.findIndex(d=>d.id===detailID);
+  if(detailIndex === -1){
       return res.status(404).json({error:"Detail not found"});}
-  book.details.splice(detailIndex,1);
-  res.status(204).send();
+  book.details.splice(detailIndex, 1);
+  res.status(200).json({message:"Successfully deleted"});
 });
 
 app.listen(port,()=>{
