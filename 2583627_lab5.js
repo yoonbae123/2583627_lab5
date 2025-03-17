80673578c18bb0ec822536edc68d6ec9e6bed827
YB <2583627@students.wits.ac.za>
2025-03-17 15:27:23 +0200

    final?

--- 2583627_lab5.js
+++ 2583627_lab5.js
@@ -11,11 +11,11 @@ let books = [];
 app.get('/whoami',(req,res) => {
     res.status(200).json(whoami);
 })
-app.get('/books', (req, res) => {
+app.get('/books',(req, res) => {
   res.status(200).json(books);
 });
 
-app.get('/books/:id', (req, res) => {
+app.get('/books/:id',(req, res) => {
   const bookID = parseInt(req.params.id);
   const book = books.find(b => b.id === bookID);
   if (!book) {
@@ -24,7 +24,7 @@ app.get('/books/:id', (req, res) => {
   res.status(200).json(book);
 });
 
-app.post('/books', (req, res) => {
+app.post('/books',(req, res) => {
   const newBook = req.body;
   newBook.id = books.length + 1;
   newBook.details = newBook.details || []; 
@@ -32,21 +32,23 @@ app.post('/books', (req, res) => {
   res.status(201).json(newBook);
 });
 
-app.put('/books/:id', (req, res) => {
+app.put('/books/:id',(req, res) => {
   const bookID = parseInt(req.params.id);
   const bookIndex = books.findIndex(c => c.id === bookID);
   if (bookIndex===-1) {
     return res.status(404).json({ error: "not found" });
+  }else{
+    const book = req.body;
+    books[bookIndex] = book;
+    res.status(200).json(book);
   }
-  books[bookIndex] = { ...books[bookIndex], ...req.body, id: bookID };
-  res.status(200).json(books[bookIndex]);
 });
 
 app.delete('/books/:id', (req, res) => {
   const bookID = parseInt(req.params.id);
   const bookIndex = books.findIndex(b => b.id === bookID);
   if (bookIndex === -1) {
-    return res.status(404).json({ error: "not found" });
+    return res.status(404).json({error: "not found"});
   }
   books.splice(bookIndex, 1);
   res.status(204).send(); 
@@ -56,11 +58,10 @@ app.post('/books/:id/details', (req, res) => {
   const bookID = parseInt(req.params.id);
   const book = books.find(b => b.id === bookID);
   if (!book) {
-    return res.status(404).json({ error: "not found" });
-  }
+    return res.status(404).json({error:"not found"});}
   const newDetail = req.body;
-  book.specs.push(newBook);
-  res.status(201).json(newBook);
+  book.details.push(newDetail);
+  res.status(201).json(newDetail);
 });
 
 app.delete('/books/:id/details/:detailID', (req, res) => {
